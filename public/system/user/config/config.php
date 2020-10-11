90b83bcb3925c5aeeb91a377ccb2f8bb845b1581
Michael Johnson <cbmmd42@gmail.com>
2020-10-11 05:53:09 +0000

    reinstalled ee

---
+++ public/system/user/config/config.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// ExpressionEngine Config Items
+// Find more configs and overrides at
+// https://docs.expressionengine.com/latest/general/system_configuration_overrides.html
+
+$config['app_version'] = '5.3.2';
+$config['encryption_key'] = '9442806367104955059d21915e788c5a8a96205d';
+$config['session_crypt_key'] = 'f4714148dbba665fcd8a4636b6f82823a4f587d6';
+$config['database'] = array(
+	'expressionengine' => array(
+		'hostname' => 'mj_website_db',
+		'database' => 'ee_prod',
+		'username' => 'michael',
+		'password' => 'xTNTYNyZGTUK2AaldsASDF',
+		'dbprefix' => 'exp_',
+		'char_set' => 'utf8mb4',
+		'dbcollat' => 'utf8mb4_unicode_ci',
+		'port'     => ''
+	),
+);
+
+// EOF
